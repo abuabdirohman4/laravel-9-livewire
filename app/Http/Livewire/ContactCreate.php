dfe42f0f09471d2666033fcfa3d590fc77b8347d
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-08-28 19:43:24 +0700

    Eps 5 Menyimpan Data Dari Form Ke Database

--- app/Http/Livewire/ContactCreate.php
+++ app/Http/Livewire/ContactCreate.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Contact;
 use Livewire\Component;
 
 class ContactCreate extends Component
@@ -13,8 +14,36 @@ class ContactCreate extends Component
     //     $this->contacts = $contacts;
     // }
 
+    public $name;
+    public $phone;
+
     public function render()
     {
         return view('livewire.contact-create');
+    }
+
+    public function store()
+    {
+        Contact::create([
+            'name' => $this->name,
+            'phone' => $this->phone
+        ]);
+        
+        $this->resetInput();
+        
+        $this->emit('contactStored');
+
+        // Kirim parameter
+        // $this->emit('contactStored', $contact);
+        // $contact = Contact::create([
+        //     'name' => $this->name,
+        //     'phone' => $this->phone
+        // ]);
+    }
+
+    public function resetInput()
+    {
+        $this->name = NULL;
+        $this->phone = NULL;
     }
 }
--- app/Http/Livewire/ContactIndex.php
+++ app/Http/Livewire/ContactIndex.php
@@ -7,11 +7,22 @@ use Livewire\Component;
 
 class ContactIndex extends Component
 {
+    protected $listeners = [
+        // 'contactStored' => 'handleStored'
+        'contactStored'
+    ];
+
     public function render()
     {
         // $this->data = Contact::latest()->get();
         return view('livewire.contact-index', [
             "contacts" => Contact::latest()->get()
         ]);
+    }
+
+    // public function handleStored($contact)
+    public function contactStored()
+    {
+        // dd($contact);
     }
 }
